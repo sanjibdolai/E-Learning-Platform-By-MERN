fb59cf8b47606da66383e1fab00a82748e7c1b09
Sanjib Dolai <sanjibdolai99.sd@gmail.com>
2022-02-26 11:43:50 +0530

    React App Create
    React App create with react-router-dom and bootstrap.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar/>}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
