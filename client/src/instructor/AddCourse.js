e73b0033b5b60ffe15b4e90dc0cb2b69df6910e3
Sanjib Dolai <sanjibdolai99.sd@gmail.com>
2022-03-10 20:17:03 +0530

    Update AddCourse.js

--- client/src/instructor/AddCourse.js
+++ client/src/instructor/AddCourse.js
@@ -1,5 +1,5 @@
 import { memo, useState, useRef } from "react";
-import { Container, Col, Row, Form } from "react-bootstrap";
+import { Container, Col, Row, Form, InputGroup, Button } from "react-bootstrap";
 import { CKEditor } from 'ckeditor4-react';
 import Select from 'react-select';
 import Cropper from "react-cropper";
@@ -25,8 +25,8 @@ function AddCourse() {
     };
     const cropperRef = useRef();
     const onCrop = () => {
-        const imageElement= cropperRef?.current;
-        const cropper= imageElement?.cropper;
+        const imageElement = cropperRef?.current;
+        const cropper = imageElement?.cropper;
         //console.log(cropper.getCroppedCanvas().toDataURL());
     };
 
@@ -53,6 +53,25 @@ function AddCourse() {
 
                 </Col>
                 <Col lg={6}>
+                    <Form.Group as={Row} className="mb-3">
+                        <Form.Label>Course Price</Form.Label>
+                        <Col xs="4" className="pt-2">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                <label class="form-check-label" for="inlineRadio1">Free</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                <label class="form-check-label" for="inlineRadio2">Paid</label>
+                            </div>
+                        </Col>
+                        <Col xs="6">
+                            <InputGroup>
+                                <Form.Control type="text" placeholder="Enter Course Price" />
+                                <InputGroup.Text id="basic-addon1"><i class="fa-solid fa-rupee-sign"></i></InputGroup.Text>
+                            </InputGroup>
+                        </Col>
+                    </Form.Group>
                     <Form.Group controlId="formFile" className="mb-3">
                         <Form.Label>Course Image</Form.Label>
                         <input className="form-control"
@@ -61,23 +80,27 @@ function AddCourse() {
                             onChange={imageChange}
                         />
                     </Form.Group>
-                    <Container style={{height:"300px"}}>
-                    {selectedImage &&
-                        <Cropper
-                            src={URL.createObjectURL(selectedImage)}
-                            style={{ height: "100%", width: "100%" }}
-                            aspectRatio={16 / 9}
-                            guides={true}
-                            crop={onCrop}
-                            ref={cropperRef}
-                        />
-                    }
+                    <Container style={{ height: "300px" }}>
+                        {selectedImage &&
+                            <Cropper
+                                src={URL.createObjectURL(selectedImage)}
+                                style={{ height: "100%", width: "100%" }}
+                                aspectRatio={16 / 9}
+                                guides={true}
+                                crop={onCrop}
+                                ref={cropperRef}
+                            />
+                        }
                     </Container>
-                    
+
                 </Col>
             </Row>
-            <Row className="border p-2 mt-2"> 
-                <Col>
+            <Row className="border p-2 mt-2">
+                <Col lg={6}>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Chapter/ Topic Title</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Title Here..." />
+                    </Form.Group>
                     <Form.Group>
                         <Form.Label>Course Content</Form.Label>
                         <CKEditor
@@ -88,8 +111,43 @@ function AddCourse() {
                         />
                     </Form.Group>
                 </Col>
+                <Col lg={6}>
+                <Form.Group as={Row} className="mb-3">
+                        <Form.Label>Videos Upload</Form.Label>
+                        <Col xs='9'>
+                        <Form.Control type="file" />
+                        </Col>
+                        <Col xs='3'>
+                            <Button variant="outline-info" size="sm"><i class="fa fa-plus"></i> Add More</Button>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3">
+                        <Form.Label>Notes Upload</Form.Label>
+                        <Col xs='9'>
+                        <Form.Control type="file" />
+                        </Col>
+                        <Col xs='3'>
+                            <Button variant="outline-info" size="sm"><i class="fa fa-plus"></i> Add More</Button>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" >
+                        <Form.Label>Important Links</Form.Label>
+                        <Col xs='9'>
+                            <Form.Control type="text" placeholder="Link Paste Here..." />
+                        </Col>
+                        <Col xs='3'>
+                            <Button variant="outline-info" size="sm"><i class="fa fa-plus"></i> Add More</Button>
+                        </Col>
+
+                    </Form.Group>
+                </Col>
             </Row>
-        </Container>
+            <Row>
+                <Col>
+                <Button variant="info" ><i class="fa fa-plus"></i> Add A New Chapter</Button>
+                </Col>
+            </Row>
+        </Container >
     );
 }
 export default memo(AddCourse);
