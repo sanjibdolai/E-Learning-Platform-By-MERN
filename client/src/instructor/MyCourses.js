17abf142477e18a12462f25b4add13cf96b40599
Sanjib Dolai <sanjibdolai99.sd@gmail.com>
2022-03-14 09:32:34 +0530

    ...

--- client/src/instructor/MyCourses.js
+++ client/src/instructor/MyCourses.js
@@ -1,7 +1,12 @@
+import { Row } from "react-bootstrap";
+
+
 function MyCourses(props) {
+    
     return(
         <>
-        
+        <Row></Row>
         </>
     );
 }
+export default MyCourses;
